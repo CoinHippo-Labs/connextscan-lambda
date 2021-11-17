ec7638f7d3c46b97f11ffa7561da2b2d780e1388
Sirapop Na Ranong <sirapop@Sirapops-MacBook-Air.local>
2021-11-17 08:04:23 +0700

    add sendingTxCount

--- crawler/index.js
+++ crawler/index.js
@@ -72,11 +72,12 @@ exports.handler = async (event, context, callback) => {
           query: `
             {
               dayMetrics(orderBy: dayStartTimestamp, orderDirection: desc, skip: ${skip}, first: ${size}) {
-                id,
-                dayStartTimestamp,
-                assetId,
-                volume,
+                id
+                dayStartTimestamp
+                assetId
+                volume
                 txCount
+                ${!version ? 'sendingTxCount' : ''}
               }
             }
           `,
@@ -94,6 +95,7 @@ exports.handler = async (event, context, callback) => {
               dayStartTimestamp: Number(dayMetric.dayStartTimestamp),
               volume: dayMetric.volume,
               txCount: Number(dayMetric.txCount),
+              sendingTxCount: dayMetric.sendingTxCount ? Number(dayMetric.sendingTxCount) : undefined,
               chain_id: chain.id,
               version,
             };
@@ -102,7 +104,7 @@ exports.handler = async (event, context, callback) => {
           for (let k = 0; k < data.length; k++) {
             let record = data[k];
 
-            if (record?.id && record.assetId && record.txCount > 0 && record.volume) {
+            if (record?.id && record.assetId && (record.txCount > 0 || record.sendingTxCount > 0) && record.volume) {
               let contract = contracts[chain.id]?.find(_contract => _contract.contract_address === record.assetId);
 
               if (!contract) {
@@ -116,7 +118,7 @@ exports.handler = async (event, context, callback) => {
               }
               record = { ...record, id: `${chain.id}-${record.id}`, normalize_volume: contract?.contract_decimals && typeof contract?.prices?.[0].price === 'number' && (Number(record.volume) * contract.prices[0].price / Math.pow(10, contract.contract_decimals)) };
 
-              if (record?.txCount > 0 && record?.normalize_volume > 0) {
+              if (record.normalize_volume > 0) {
                 // send request
                 const a = await opensearcher.post('', { ...record, index: env.index_name, method: 'update', id: record.id })
                   // set response data from error handled by exception
