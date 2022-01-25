fe99b3ac08ecd1b46c1a5949d896994c6007831f
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-01-25 14:54:59 +0700

    update config

--- requester/bridge_config/index.js
+++ requester/bridge_config/index.js
@@ -1035,7 +1035,6 @@ const assets = [
   {
     id: 'fei',
     symbol: 'FEI',
-    is_staging: true,
     contracts: [
       {
         contract_address: '0x956f47f50a910163d8bf957cf5846d573e7f87ca',
