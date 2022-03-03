23ad18f6aff39e26d872a0ec84e13a4e64ae6813
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-03-03 18:52:00 +0700

    change name

--- requester/index.js
+++ requester/index.js
@@ -149,9 +149,9 @@ exports.handler = async (event, context, callback) => {
     },
     crosschain_config: {},
     bridge_config: {
-      git_repo: process.env.BRIDGE_CONFIG_GIT_REPO || 'CoinHippo-Labs/connext-network-xpollinate',
+      git_repo: process.env.BRIDGE_CONFIG_GIT_REPO || 'CoinHippo-Labs/connext-network-bridge',
       s3_url: process.env.BRIDGE_CONFIG_S3_URL || 'https://s3.us-west-1.amazonaws.com',
-      s3_bucket: process.env.BRIDGE_CONFIG_S3_BUCKET || 'config.xpollinate.connext.network',
+      s3_bucket: process.env.BRIDGE_CONFIG_S3_BUCKET || 'config.bridge.connext.network',
     },
   };
 
