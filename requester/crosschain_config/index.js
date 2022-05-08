890a3a95f9838227575bd558ca28d2da48b3b768
Sirapop <sirapop@Sirapops-MacBook-Air.local>
2022-05-08 14:30:37 +0700

    update rpc config

--- requester/crosschain_config/index.js
+++ requester/crosschain_config/index.js
@@ -693,7 +693,7 @@ const chains_testnet = [
       {
         chainId: '0x13881',
         chainName: 'Polygon Mumbai',
-        rpcUrls: ['https://rpc-mumbai.matic.today', 'https://matic-mumbai.chainstacklabs.com', 'https://rpc-mumbai.maticvigil.com'],
+        rpcUrls: ['https://rpc-mumbai.matic.today', 'https://matic-mumbai.chainstacklabs.com'],
         nativeCurrency: {
           name: 'Matic',
           symbol: 'MATIC',
